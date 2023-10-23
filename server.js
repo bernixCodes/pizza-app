51e4267045fc23d0e84dad3af351d915272988aa
bernixCodes <adjoa.bernie@gmail.com>
2023-10-23 08:25:58 +0000

    login, register, cart screens

--- server.js
+++ server.js
@@ -9,13 +9,24 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-// app.use(expressLayout);
+app.use(expressLayout);
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("home");
 });
-  
+
+app.get("/cart", (req, res) => {
+  res.render("customers/cart");
+});
+
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("auth/register");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
